c0810c3c7789383d8f688df43264acc93d34d61d
jqb <redpandadog2023@gmail.com>
2026-07-30 16:16:20 +0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -14,9 +14,6 @@ except Exception as e:
     st.stop()
 
 def main():
-
-    if "logout_confirm_started" not in st.session_state:
-        st.session_state.logout_confirm_started = None
       
     if login_ui.handle_idle_timeout():
         return
